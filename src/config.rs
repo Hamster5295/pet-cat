1284a94653e24faea6024f22fda5379e636d6962
Hamster5295 <hamster5295@163.com>
2025-06-28 09:13:01 +0800

    init

---
+++ src/config.rs
@@ -0,0 +1,67 @@
+use std::{
+    path::PathBuf,
+    str::FromStr,
+    sync::{Arc, OnceLock},
+};
+
+use anyhow::{Context, Result};
+use kovi::{RuntimeBot, tokio::fs};
+use serde::Deserialize;
+
+pub(crate) static CONFIG: OnceLock<Config> = OnceLock::new();
+
+#[derive(Deserialize)]
+struct ConfigFile {
+    api_url: String,
+    api_key: String,
+    model: String,
+
+    prompt: Option<String>,
+    pet_cat_img: String,
+
+    allow_groups: Vec<i64>,
+}
+
+pub struct Config {
+    pub api_url: String,
+    pub api_key: String,
+    pub model: String,
+
+    pub prompt: String,
+    pub pet_cat_img: String,
+
+    pub allow_groups: Vec<i64>,
+}
+
+pub(crate) async fn init(bot: &Arc<RuntimeBot>) -> Result<&Config> {
+    let config_path = bot.get_data_path().join("config.toml");
+    let config_txt = fs::read_to_string(&config_path)
+        .await
+        .with_context(|| format!("Failed to read config file at {}", config_path.display()))?;
+    let config_file: ConfigFile = toml::from_str(&config_txt)?;
+
+    let pet_cat = PathBuf::from_str(&config_file.pet_cat_img)?;
+    let pet_cat = if pet_cat.is_absolute() {
+        pet_cat
+    } else {
+        bot.get_data_path().join(pet_cat)
+    };
+
+    if !pet_cat.exists() {
+        return Err(anyhow::anyhow!(
+            "Pet Cat Picture not found from {}",
+            config_path.display()
+        ));
+    }
+
+    return Ok(CONFIG.get_or_init(|| Config {
+        api_key: config_file.api_key,
+        api_url: config_file.api_url,
+        model: config_file.model,
+        prompt: config_file
+            .prompt
+            .unwrap_or("请辨别这张图片是否包含一只真实的猫咪，而非卡通猫咪或表情包。".to_string()),
+        pet_cat_img: pet_cat.to_string_lossy().into(),
+        allow_groups: config_file.allow_groups,
+    }));
+}
---
+++ src/lib.rs
@@ -0,0 +1,166 @@
+mod config;
+
+use kovi::{
+    Message, PluginBuilder as plugin, RuntimeBot,
+    bot::runtimebot::kovi_api::SetAccessControlList,
+    event::GroupMsgEvent,
+    log::{error, info},
+    serde_json::{Value, json},
+};
+use std::sync::{Arc, OnceLock};
+
+static BOT: OnceLock<Arc<RuntimeBot>> = OnceLock::new();
+static CLIENT: OnceLock<reqwest::Client> = OnceLock::new();
+
+#[kovi::plugin]
+async fn main() {
+    let bot = plugin::get_runtime_bot();
+    BOT.get_or_init(|| bot.clone());
+    CLIENT.get_or_init(|| reqwest::ClientBuilder::new().build().unwrap());
+
+    let config = config::init(&bot).await.unwrap();
+
+    bot.set_plugin_access_control("pet-cat", true).unwrap();
+    bot.set_plugin_access_control_list(
+        "pet-cat",
+        true,
+        SetAccessControlList::Adds(config.allow_groups.clone()),
+    )
+    .unwrap();
+    plugin::on_group_msg(on_msg);
+
+    info!("Ready to pet cats!");
+}
+
+async fn on_msg(event: Arc<GroupMsgEvent>) {
+    let imgs = event.message.get("image");
+
+    for img in imgs {
+        let map = img.data.as_object();
+        if let None = img.data.as_object() {
+            info!("No data provided by image segment. (Strange!)");
+            continue;
+        }
+
+        let url = map.unwrap().get("url");
+        if let None = url {
+            info!("No url provided by image segment. (Strange!)");
+            continue;
+        }
+
+        let mut url = url.unwrap().as_str().unwrap().to_string();
+        if url.starts_with("https") {
+            url = url.replace("https", "http");
+        }
+        info!("Image url: {}", url);
+
+        if predict_cat(&url).await {
+            info!("Cat detected, sending pet cat meme...");
+            send_pet_cat(event.group_id).await;
+        } else {
+            info!("No cat detected.")
+        }
+    }
+}
+
+async fn predict_cat(url: &str) -> bool {
+    let config = config::CONFIG.get().unwrap();
+    let client = CLIENT.get().unwrap();
+
+    info!("Predicting cat for image: {}", url);
+
+    let req = match client
+        .post(&config.api_url)
+        .bearer_auth(&config.api_key)
+        .json(&json!({
+            "model": config.model,
+            "messages": [
+                {
+                    "role": "system",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "你是一个专业的图片分辨专家，可以精确地依据用户的指示，分辨图片中是否包含某一特定物体。**你只能回答 是 或 否，不要做出多余的回答或进行解释**。"
+                        }
+                    ]
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": url
+                            }
+                        },
+                        {
+                            "type": "text",
+                            "text": config.prompt
+                        }
+                    ]
+                }
+            ]
+        }))
+        .build(){
+            Ok(req) => req,
+            Err(e) => {
+                error!("Failed to build request: {}", e);
+                return false;
+            }
+        };
+
+    let resp = match client.execute(req).await {
+        Ok(resp) => resp,
+        Err(e) => {
+            error!("Failed to get response: {}", e);
+            return false;
+        }
+    };
+
+    let resp: Value = match resp.json().await {
+        Ok(resp) => resp,
+        Err(e) => {
+            error!("Failed to parse response: {}", e);
+            return false;
+        }
+    };
+
+    let resp = resp.as_object().unwrap();
+
+    info!("{:?}", resp);
+
+    let Some(result) = resp.get("choices") else {
+        info!("Invalid response: {:?}", resp);
+        return false;
+    };
+
+    let Some(result) = result.as_array().unwrap().get(0) else {
+        info!("No choice provided: {:?}", resp);
+        return false;
+    };
+
+    let result = result["message"]["content"].as_str();
+
+    if let Some(s) = result {
+        return s.trim() == "是";
+    }
+
+    false
+}
+
+async fn send_pet_cat(group: i64) {
+    let bot = BOT.get().unwrap();
+    let config = config::CONFIG.get().unwrap();
+    bot.send_group_msg(
+        group,
+        Message::from_value(json!([
+            {
+                "type":"image",
+                "data": {
+                    "file": config.pet_cat_img,
+                }
+            }
+        ]))
+        .unwrap(),
+    );
+}
